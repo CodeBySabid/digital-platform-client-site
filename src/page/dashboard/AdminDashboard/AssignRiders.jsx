638c4fa0c29591129be91b6edddee0d4718dc613
Sabid <sajjadsabid0@gmail.com>
2026-01-20 04:58:56 +0600

    Assign Riders page model has been added, where rider information is displayed and rider data is fetched.

--- src/page/dashboard/AdminDashboard/AssignRiders.jsx
+++ src/page/dashboard/AdminDashboard/AssignRiders.jsx
@@ -1,9 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import UseAxiosSecure from '../../../hooks/UseAxiosSecure';
 
 const AssignRiders = () => {
+    const [selectedParcel, setSelectedParcel] = useState(null)
     const axiosSecure = UseAxiosSecure();
+    const riderModalRef = useRef();
+    const formateDate = (dateString) => {
+        return new Date(dateString).toISOString().split('T')[0]
+    }
     const { data: parcels = [] } = useQuery({
         queryKey: ['parcels', 'pending-pickup'],
         queryFn: (async () => {
@@ -11,6 +16,31 @@ const AssignRiders = () => {
             return res.data
         })
     })
+    const { data: riders = [] } = useQuery({
+        queryKey: ['riders', selectedParcel?.SenderDistrict, 'Available'],
+        enabled: !!selectedParcel,
+        queryFn: (async () => {
+            const res = await axiosSecure.get(`/riders?status=Approved&district=${selectedParcel.SenderDistrict}&
+            workStatus=Available`);
+            return res.data
+        })
+    })
+
+    const openAssignRiderModal = parcel => {
+        setSelectedParcel(parcel);
+        riderModalRef.current.showModal();
+    }
+
+    const handleAssignRider = (rider) => {
+        const riderInfo = {
+            riderId: rider._id,
+            riderEmail: rider.email,
+            riderName: rider.name,
+            parcelId: selectedParcel?._id,
+        }
+        console.log(riderInfo)
+        // axiosSecure.patch(`riders`, riderInfo)
+    }
     return (
         <div>
             <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-green-500 font-semibold mt-2 text-center '>Assign Riders {parcels.length}</h1>
@@ -38,22 +68,57 @@ const AssignRiders = () => {
                                         <td>{parcel.parcelName}</td>
                                         <td>{parcel.senderRegions} <br /> {parcel.SenderDistrict}</td>
                                         <td>{parcel.ReceiverRegions} <br /> {parcel.ReceiverDistrict}</td>
-                                        <td className={`${parcel.workStatus === 'Available' ? 'text-green-600 font-bold' : "text-red-500"}`}>{parcel.workStatus}</td>
                                         <td>{parcel.deliveryStatus}</td>
                                         <td>{parcel.cost}</td>
                                         <td>{parcel.paymentStatus === 'paid' ? <span className='text-black font-semibold bg-[#5edb24] p-2 rounded'>Paid</span> :
                                             <button className='btn bg-[#CAEB66] text-black'>Pay</button>
                                         }</td>
-                                        <td>{parcel.createAt}</td>
-                                        <td className='flex gap-2'>
-                                            <button className='btn bg-[#717bd6]'>Assign Rider</button>
+                                        <td>{formateDate(parcel.createAt)}</td>
+                                        <td>
+                                            <button onClick={() => openAssignRiderModal(parcel)} className='btn bg-[#717bd6]'>Assign Rider</button>
                                         </td>
-                                    </tr>)
+                                    </tr>
+                                )
                             }
                         </tbody>
                     </table>
                 </div>
             </div>
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <dialog ref={riderModalRef} className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Riders: {riders.length} !</h3>
+                    <form method="dialog">
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        <div className="overflow-x-auto">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        riders.map((rider, index) =>
+                                            <tr key={rider._id}>
+                                                <th>{index + 1}</th>
+                                                <td>{rider.name}</td>
+                                                <td>{rider.email} </td>
+                                                <td>
+                                                    <button onClick={() => handleAssignRider(rider)} className='btn bg-[#717bd6]'>Assign Rider</button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </form>
+                </div>
+            </dialog>
         </div>
     );
 };
